a046d2c0540c8c2dd99ccf65318f646559d73341
richietam <richardtam.ny@gmail.com>
2019-06-04 18:49:02 -0400

    Change to config initializer aws.rb

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,5 +1,5 @@
 S3Client = Aws::S3::Client.new(
-    access_key_id: ENV['ACCESS_KEY_ID'],
-    secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: 'us-east-1'
 )
